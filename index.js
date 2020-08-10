fbd0189ba17f08595791e691d988017d31e4ff3a
M Ahmed Mushtaq <mmushtaq.bese19seecs@seecs.edu.pk>
2020-08-10 22:44:28 +0500

    Job description scrapper

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const request = require('request-promise');
-const cheerio = require("cheerio");
+const cheerio = require('cheerio');
 
-const url = "https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof";
+const url = 'https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof';
 
-(async ()=>{
-    try{
-        const htmlResult = await request.get(url);
-        console.log(htmlResult);
-    }catch(err){
-        console.error(err);
-    }
-})()
+const scrapResults = [];
+
+const scrapeJobsHeader = async () => {
+  try {
+    const htmlResult = await request.get(url);
+    const $ = cheerio.load(htmlResult);
+
+    $('.result-info').each((index, element) => {
+      const resultTitle = $(element).children('.result-title');
+      const title = resultTitle.text();
+      const url = resultTitle.attr('href');
+      const datePosted = new Date($(element).children('time').attr('datetime'));
+      const hood = $(element).find('.result-hood').text();
+
+      const scrapResult = { title, url, datePosted, hood };
+      scrapResults.push(scrapResult);
+    });
+
+    return scrapResults;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const scrapeDescription = async (jobsWithHeader) => {
+  await Promise.all(
+    jobsWithHeader.map(async (job) => {
+      const htmlResult = await request.get(job.url);
+      const $ = cheerio.load(htmlResult);
+    })
+  );
+};
+
+(async () => {
+  const jobsWithHeader = await scrapeJobsHeader();
+  const jobsFullData = await scrapeDescription(jobsWithHeader);
+})();
